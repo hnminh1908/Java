4ffd3fe91ffccdcd784915e3db5bc0b3242071de
hnminh1908 <100814090+hnminh1908@users.noreply.github.com>
2022-11-10 23:52:24 +0700

    Add files via upload

---
+++ StrategyDesignPattern/StrategyPattern.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package strategypattern;
+
+/**
+ *
+ * @author Admin
+ */
+public class StrategyPattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+        
+        context=new Context(new OperationSubtract());
+        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+        
+        context = new Context(new OperationMultiply());
+        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+    }
+    
+}
